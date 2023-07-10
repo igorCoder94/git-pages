fd5fdb804117c188c25cdb1934f3cc6c398dc19d
igorCoder94 <sonic-new78@yandex.ru>
2023-07-10 11:11:28 +0300

    asd

--- src/pages/pageMain/registration/index.jsx
+++ src/pages/pageMain/registration/index.jsx
@@ -6,10 +6,10 @@ const Registration = () => {
             <div className='registration__title'>Регистрация</div>
             <div className='registration__description'>Зарегистрируйтеся прямо сейчас и опробуйте<br />ограниченную версию платформы.</div>
             <a className='registration__link' href="https://go.unios.io">Регистрация</a>
-            <div className='registration__border-left'></div>
-            <div className='registration__border-left-center'></div>
-            <div className='registration__border-right-center'></div>
-            <div className='registration__border-right'></div>
+            <div className='registration__border registration__border-left'></div>
+            <div className='registration__border registration__border-left-center'></div>
+            <div className='registration__border registration__border-right-center'></div>
+            <div className='registration__border registration__border-right'></div>
         </div>
     </section>
 }
