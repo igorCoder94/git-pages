60e1d88987ed5227660cc8adf9effffe92cd1326
igorCoder94 <sonic-new78@yandex.ru>
2023-07-14 11:25:32 +0300

    add meta tags

--- src/pages/pageDispatching/index.jsx
+++ src/pages/pageDispatching/index.jsx
@@ -4,10 +4,15 @@ import Integration from './integration';
 import SolutionsDim from './solutions-dim';
 import SolutionService from './solutions-service';
 import Scheme from './scheme';
+import { Helmet } from 'react-helmet';
 
 const PageDispatching = () => {
     return (
         <>
+            <Helmet>
+                <title>Модуль диспетчеризации и мониторинга</title>
+                <meta name="description" content="Сбор и анализ информации о состоянии оборудования, представление её в удобном виде. Промышленная видео аналитика, определение аварийных ситуаций. Интеграция с системами электронного документооборота" />
+            </Helmet>
             <Hero />
             <SolutionsDim />
             <SolutionService />
--- src/pages/pageMain/index.jsx
+++ src/pages/pageMain/index.jsx
@@ -5,6 +5,7 @@ import News from './news';
 import Registration from './registration';
 import HeroMobile from './hero-mobile';
 import { useMediaQuery } from '@react-hook/media-query';
+import { Helmet } from 'react-helmet';
 
 const PageMain = () => {
     const isMobile = useMediaQuery('(max-width: 800px)');
@@ -13,6 +14,11 @@ const PageMain = () => {
 
     return (
         <>
+            <Helmet>
+                <title>Unios - Сервис для автоматизации эксплуатации объектов</title>
+                <meta name="description" content="Unios - сервис автоматизации эксплуатации и технического обслуживания оборудования. Мониторинг, Диспетчеризация, Техническое обслуживание и Ремонт оборудования" />
+                <meta name="keywords" content="автоматизация, IoT, ТОиР, диспетчеризация, мониторинг, сервис, эксплуатация, обслуживание, оборудование, Эф-Ти-Икс-Ком, FTXCom, Unios, Юниос, ЭФ ТИ ИКС КОМ, SCADA, API, CCTV, видеонаблюдение, аварийные ситуации, устройство, объект" />
+            </Helmet>
             {isMobile ? <HeroMobile /> : <Hero />}
             <Services />
             <Advantages />
